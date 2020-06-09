eb41a36ce6a3def179b0e2e64126fc909d202d67
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-09 15:30:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word (sentence)
-  sentence_array = [sentence.reverse]
+  sentence_array = [sentence]
   
-  
+  sentence_array.collect do |element|
+    element.reverse 
+  end
+  sentence_array[0]
 end
