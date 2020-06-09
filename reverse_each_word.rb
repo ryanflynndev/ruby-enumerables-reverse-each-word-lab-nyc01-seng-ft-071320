2acbe29a3c60edaf840ac2c191f1996142d8ac2f
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-09 15:47:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,13 @@ def reverse_each_word (sentence)
   sentence_array = sentence.split
   answer = ""
   sentence_array.collect do |element|
+    if sentence_array[element] == sentence_array[sentence_array.length - 1]
+      answer += element.reverse
     
-    answer += element.reverse + " "
+    else 
+      answer += element.reverse + " "
+    end
   end
   answer
 end
 
-#reverse_each_word("Hello, how are you?")
