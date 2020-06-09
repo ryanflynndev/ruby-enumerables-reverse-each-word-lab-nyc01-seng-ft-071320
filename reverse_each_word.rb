6a6ef67dcfdda6d4110d94733f19b09989ea1df3
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-09 15:48:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,14 @@
 def reverse_each_word (sentence)
   sentence_array = sentence.split
   answer = ""
+  count = 0
   sentence_array.collect do |element|
-    if sentence_array[element] == sentence_array[sentence_array.length - 1]
+    if sentence_array[count] == sentence_array[sentence_array.length - 1]
       answer += element.reverse
-    
+      count += 1
     else 
       answer += element.reverse + " "
+      count += 1
     end
   end
   answer
